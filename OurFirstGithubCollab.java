4158a9c0af3d908e15bbf147bc1aebb610dca31c
unknown <rhopaloceus@gmail.com>
2023-09-13 16:12:06 +0700

    rawr praja

--- OurFirstGithubCollab.java
+++ OurFirstGithubCollab.java
@@ -6,5 +6,6 @@ public class OurFirstGithubCollab{
             System.out.println("Tambahan dari anggota 1");
             System.out.println("Aku mau makan");
             System.out.println("Terserah");
+            System.out.println("Inshallah");
         }
 }

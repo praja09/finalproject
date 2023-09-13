135f63b375a94b43ba368bf4785a4bcd92c20815
praja09 <dipaprajadpp@gmail.com>
2023-09-13 13:08:29 +0700

    fitur 2

--- OurFirstGithubCollab.java
+++ OurFirstGithubCollab.java
@@ -1,5 +1,7 @@
 public class OurFirstGithubCollab{
         public static void main(String[] args){
             System.out.println(x:"Ini ceritanya fitur pertama sudah berjalan baik");
+
+            System.out.println("Ketua mulai mengembangkan fitur kedua");
         }
 }

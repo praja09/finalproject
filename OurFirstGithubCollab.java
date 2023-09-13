287d254bb1f7fa33ec951e8320b2f7379634d448
fasyadita <fasyadita1@gmail.com>
2023-09-13 14:40:35 +0700

    perubahan main

--- OurFirstGithubCollab.java
+++ OurFirstGithubCollab.java
@@ -4,6 +4,5 @@ public class OurFirstGithubCollab{
 
             System.out.println("Ketua mulai mengembangkan fitur kedua");
             System.out.println("Tambahan dari anggota 1");
-            system.out.println(x:"ayo besok kerja kelompok");
         }
 }
